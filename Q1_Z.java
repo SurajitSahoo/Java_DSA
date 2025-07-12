a9325ba12a02cf8eb25c64c7b42ab4bde9b8ebe2
Surajit Sahoo <surajitsahoo200@gmail.com>
2025-07-12 11:01:35 +0530

    print z

---
+++ Q1_Z.java
@@ -0,0 +1,10 @@
+//Print Z using *
+public class Q1_Z {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");
+    }
+}
